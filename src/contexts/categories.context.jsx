317a2c66a881564b337b6ff6895024b9f57fcfa9
Erik Jonsberg <erikj@hungryghostdesign.com>
2022-08-30 10:14:08 -0400

    refactored products context into categories context, imported data from firebase to shop page

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,25 @@
+import {createContext, useState, useEffect} from 'react';
+
+import {getCategoriesAndDocuments} from '../utils/firebase/firebase.utils'
+//import SHOP_DATA from '../shop-data.js'
+
+// actual value to be accessed
+export const CategoriesContext = createContext({
+    categoriesMap: {},
+});
+
+export const CategoriesProvider = ({children}) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
+     useEffect(() => {
+        const getCategoriesMap = async () => {
+        const categoryMap = await getCategoriesAndDocuments();
+        console.log(categoryMap);
+        setCategoriesMap(categoryMap)
+        }
+        getCategoriesMap();
+     }, [])
+
+    const value = {categoriesMap};
+
+    return <CategoriesContext.Provider value={value} >{children}</CategoriesContext.Provider>
+}
