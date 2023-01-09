9c9c6d557f8cda71fdf4ff59a1145e5aae711246
Erik Jonsberg <erikj@hungryghostdesign.com>
2023-01-09 13:58:52 -0500

    Typed redux store and root reducer

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import ShoppingIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import {selectIsCartOpen} from '../../store/cart/cart.selector.js';
+import {selectIsCartOpen} from '../../store/cart/cart.selector';
 import { selectCurrentUser } from '../../store/user/user.selector';
 import { signOutStart } from '../../store/user/user.action';
 
